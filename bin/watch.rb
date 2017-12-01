ab8df8ed91ec169e46a4070034227c6e102a2b87
Mark Hammonds <markhammonds@Paragon.local>
2017-12-01 18:06:53 -0500

    Adding liquid and delaying reload.

--- bin/watch.rb
+++ bin/watch.rb
@@ -11,7 +11,7 @@ if ARGV.length < 2
 end
 
 dev_extension = 'dev'
-filetypes = ['go', 'css','html','htm','php','rb','erb','less','js','py']
+filetypes = ['go', 'css','html','htm','php','rb','erb','less','js','py', 'liquid']
 watch_folder = ARGV[0]
 keyword = ARGV[1]
 puts "Watching #{watch_folder} and subfolders for changes in project files..."
@@ -31,7 +31,7 @@ while true do
     diff_hash.each do |df|
       puts "Detected change in #{df[0]}, refreshing"
       %x{osascript<<ENDGAME
-          delay 1
+          delay 2
           tell application "Google Chrome"
               set windowList to every window
               repeat with aWindow in windowList
